50b926536f83cd2c9ba5a891be8a0eb0a54f8f29
Igor Shilyaev <ishilyaev46@gmail.com>
2026-04-21 20:03:34 +0400

    Edited configs.

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const config = getDefaultConfig(__dirname);
-const documentPickerPath = path.resolve(__dirname, '..', 'node_modules', 'expo-document-picker');
-
-config.watchFolders = [documentPickerPath];
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  'expo-document-picker': documentPickerPath,
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
