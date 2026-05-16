86739c4d5dda4f946b9491fdddd38af3710ce300
Alexander Gerhard <870717ag@gmail.com>
2026-05-16 19:47:45 +0200

    style: improve layout spacing and footer positioning

--- src/screens/Main/HomeVariants/HomeVariant2.styles.js
+++ src/screens/Main/HomeVariants/HomeVariant2.styles.js
@@ -2,6 +2,42 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#3097F7",
+    paddingVertical: 60,
+    paddingHorizontal: 20,
     flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
+  },
+
+  main: {
+    marginTop: 30,
+    flex: 1,
+    justifyContent: "space-between",
+
+  },
+
+  projectSelector: {
+    alignItems: "center",
+  },
+
+  timer: {
+    alignItems: "center",
+  },
+
+  mainActionButtons: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: "auto",
   },
 });
